238bab28d9a90b45c3ffe21749d0c9dce45f2f6e
anundbg89 <42108126+anundbg89@users.noreply.github.com>
2022-10-15 20:37:30 +0530

    final commit to check now

--- App.js
+++ App.js
@@ -1,4 +1,38 @@
-var express = require("express");
+/*var express = require("express");
 var app = express();
 app.use(express.static(__dirname + "/"));
-const initialfunc = require("./CodeBlock/node/final.js");
+
+var server = http.createServer(app)
+console.log('port is'+ port);
+server.listen(port)
+app.use(function (req, res, next) {
+    
+    next()
+  })
+  require('dns').lookup(require('os').hostname(), function (err, add, fam) {
+    console.log('addr: ' + add);
+  })*/
+  const {encryptDataFunc,} = require("./CodeBlock/node/final.js");
+  
+  var http = require("http")
+  var express = require("express")
+  var app = express()
+  var port = process.env.PORT || 7000
+  var bodyParser = require('body-parser')
+  app.use(bodyParser.json());
+  var server = http.createServer(app)
+console.log('port is'+ port);
+server.listen(port)
+  app.use(function (req, res, next) {
+    console.log(req.body.stringToEncrypt);
+    console.log(req.body.requesterData);
+    encryptDataFunc(req.body.stringToEncrypt,req.body.requesterData);
+    //const fideliusVersion = getFideliusVersion()
+
+    //initialfunc(initialfunc)
+    
+    next()
+  })
+  require('dns').lookup(require('os').hostname(), function (err, add, fam) {
+    console.log('addr: ' + add);
+  })
--- CodeBlock/node/final.js
+++ CodeBlock/node/final.js
@@ -9,6 +9,7 @@ const {
 	ensureDirExists,
 } = require("./utils.js");
 
+
 const fideliusVersion = getFideliusVersion();
 const binPath = path.join(
 	__dirname,
@@ -98,8 +99,40 @@ const runExample = ({ stringToEncrypt,requesterData }) => {
 	);
 };
 
+module.exports = {
+	encryptDataFunc : function encryptDataFunc ( stringToEncrypt,requesterData ) {
+		const senderKeyMaterial = getEcdhKeyMaterial();
+		const requesterKeyMaterial = requesterData;
+	
+		
+		const encryptionResult = encryptData({
+			stringToEncrypt,
+			senderNonce: senderKeyMaterial.nonce,
+			requesterNonce: requesterKeyMaterial.nonce,
+			senderPrivateKey: senderKeyMaterial.privateKey,
+			requesterPublicKey: requesterKeyMaterial.publicKey,
+		});
+		
+		console.log({ senderKeyMaterial});
+		console.log( {encryptionResult});
+		const checkval  = checksum(encryptionResult.encryptedData)
+		const checkvalbase64 = Buffer.from(checkval).toString('base64')
+	
+		console.log('checksum details' + checkvalbase64);
+		request.post(
+			'https://ndhm.herokuapp.com/',
+			{ json: { encryptData :  encryptionResult.encryptedData,senderKeyMaterial: senderKeyMaterial,chekcsumvalue: checkvalbase64, inboundUrl : "encryptdata"} },
+			function (error, response, body) {
+				if (!error && response.statusCode == 200) {
+					console.log(body);
+				}
+			}
+		);
+	},
+}
 
-		runExample( {stringToEncrypt: '{"data": "There is no war in Ba Sing Se!"}', requesterData: '{"nonce" : "mRTKi3jFnS7/HET9iKnrn0PkKf677ka16nyX+/5l8sc=","publicKey" : "BDb0S6+PsekYnhXars8ZlOpdHePGpPOBWkCqIBSws0I7d4OVNQpogxJN1p57sXWcYAv+m9zxkAdGttMP8geaIjA=" }' });
+
+//runExample( {stringToEncrypt: '{"data": "There is no war in Ba Sing Se!"}', requesterData: '{"nonce" : "mRTKi3jFnS7/HET9iKnrn0PkKf677ka16nyX+/5l8sc=","publicKey" : "BDb0S6+PsekYnhXars8ZlOpdHePGpPOBWkCqIBSws0I7d4OVNQpogxJN1p57sXWcYAv+m9zxkAdGttMP8geaIjA=" }' });
 
 
 
